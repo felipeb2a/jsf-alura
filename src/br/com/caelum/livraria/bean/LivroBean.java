2b153ce5216a410e61fc7bdf10cd5cceb138981a
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-04-10 22:55:41 -0300

    aula 03 - Entendendo MVC e integrando o banco de dados com JPA 2

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,6 +2,9 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 
+import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Livro;
+
 @ManagedBean
 public class LivroBean {
 
@@ -11,11 +14,14 @@ public class LivroBean {
 		return livro;
 	}
 
-	public void setLivro(Livro livro) {
-		this.livro = livro;
-	}
-
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
 	}
+
 }
