7cded6702174ed4df5fc28c86c7a320391b75d62
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-04-11 13:30:06 -0300

    aula 05 - Conversão e validação de dados
    - Converter data String para Calendar;
    - Adicionar pattern data no componente data, e pegar instance time java util
    - Definir time zone no web.xml

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -27,6 +31,10 @@ public class LivroBean {
 	public Livro getLivro() {
 		return livro;
 	}
+	
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
 
 	public List<Autor> getAutores() {
 		return new DAO<Autor>(Autor.class).listaTodos();
@@ -45,10 +53,22 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+//			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor"));
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
+	}
+	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException{
+		
+		String valor = value.toString();
+		if(!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
+		
 	}
 
 }
