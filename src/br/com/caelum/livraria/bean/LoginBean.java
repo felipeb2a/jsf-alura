e47d3dff03166ca23d04e841f9765972c83cc6de
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-04-12 13:35:30 -0300

    aula 12 - Video 2 Autorização pelo PhaseListener
    Verificar se o usuario autenticou com PhaseListener

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -21,6 +22,8 @@ public class LoginBean {
 		System.out.println("Fazendo login do usuario" + this.usuario.getEmail());
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if(existe ) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", usuario);
 			return "livro?faces-redirect-true";			
 		}
 		
