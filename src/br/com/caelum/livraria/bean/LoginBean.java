a29d3150e1532c9d1d8f034f9b7b189078813174
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-04-17 01:29:42 -0300

    aula 01 - Gerenciando Beans com CDI
    1 - @Managed trocar por @Named package javax.inject.Named;
    2 - @ViewScoped trocar import por javax.faces.view.ViewScoped
    3 - @SessionScoped trocar impot por javax.enterprise.context.SessionScoped

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -1,15 +1,15 @@
 package br.com.caelum.livraria.bean;
 
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
 import br.com.caelum.livraria.util.RedirectView;
 
-@ManagedBean
+@Named
 @ViewScoped
 public class LoginBean {
 
