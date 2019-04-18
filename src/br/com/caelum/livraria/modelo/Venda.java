e4db5cbc69d2622fb3a6dcbfff7039021d46574d
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-04-18 12:26:28 -0300

    aula 04 - Selecionando as vendas do banco de dados
    Criar dao vendas;
    Adicionar algumas vendas no BD;

--- src/br/com/caelum/livraria/modelo/Venda.java
+++ src/br/com/caelum/livraria/modelo/Venda.java
@@ -1,8 +1,23 @@
 package br.com.caelum.livraria.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Venda {
-	private Livro livro;
+	
+	@Id @GeneratedValue
+	private Integer id;
+	
+	@ManyToOne(fetch = FetchType.EAGER) 
+	private Livro livro;	
 	private Integer quantidade;
+	
+	public Venda() {		
+	}
 			
 	public Venda(Livro livro, Integer quantidade) {
 		super();
